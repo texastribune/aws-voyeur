f05e6b620c7d758c465d452a34a3212f63439c16
crccheck <c@crccheck.com>
2014-08-15 18:22:33 -0500

    move documentation to a readme

--- voyeur.py
+++ voyeur.py
@@ -4,16 +4,6 @@
 Quick n dirty CLI for checking ec2 inventory
 
 Specifically design for my own personal workflow.
-
-If you're doing something serious, you should look into these better
-alternatives:
-
-* https://github.com/DrGonzo65/ec2-cli-tools - very similar package that lets
-  you glob
-* https://github.com/mattrobenolt/ec2 - lets you query instances using django-
-  orm-like syntax
-* awscli - official aws cli
-
 """
 import sys
 
